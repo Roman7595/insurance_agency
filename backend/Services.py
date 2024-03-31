0c247428ceff89a942d7df0ca63e4b9f57069046
Roman7595 <126889828+Roman7595@users.noreply.github.com>
2024-03-31 20:46:08 +0300

    add: first page quaries except raw

--- backend/Services.py
+++ backend/Services.py
@@ -1,14 +1,5 @@
 from backend import Repositories as R
 
-def get_contracts_by_client(client_id):
-    values = list(R.get_contracts_by_client(client_id[0]))
-    columns = R.get_contracts_colums()
-
-    if len(values):
-        return (columns, values)
-    else:
-        return
-
 
 def raw_sql(sql):
     return
@@ -19,3 +10,53 @@ def raw_sql(sql):
     #     return (1, columns, values)
     # else:
     #     return (1)
+
+def get_contracts_by_client(client_id):
+    client_id = client_id[0]
+    values = list(R.get_contracts_by_client(client_id))
+    columns = R.get_contracts_columns()
+
+    if len(values):
+        return (columns, values)
+    else:
+        return
+
+def get_autos_by_client(client_id):
+    client_id = client_id[0]
+    values = list(R.get_autos_by_client(client_id))
+    columns = R.get_autos_columns()
+
+    if len(values):
+        return (columns, values)
+    else:
+        return
+
+def get_contracts_by_federal_region(client_id):
+    client_id = client_id[0]
+    values = list(R.get_contracts_by_federal_region(client_id))
+    columns = R.get_contracts_columns()
+
+    if len(values):
+        return (columns, values)
+    else:
+        return
+
+def get_contracts_by_time(period):
+    (start, end) = period
+    values = list(R.get_contracts_by_time(start,end))
+    columns = R.get_contracts_columns()
+
+    if len(values):
+        return (columns, values)
+    else:
+        return
+
+def get_reasons_by_contract(contract_id):
+    contract_id = contract_id[0]
+    values = list(R.get_reasons_by_contract(contract_id))
+    columns = R.get_reasons_columns()
+
+    if len(values):
+        return (columns, values)
+    else:
+        return
--- views/Sql_quary_output_view.py
+++ views/Sql_quary_output_view.py
@@ -1,61 +1,54 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+
 from utils import My_utils as u
 from views import Main_view
+
 def back_to_main_menu(window):
     u.clear_window(window)
     Main_view.fill_main_frame(window)
 
-def main(window, type,sql_query_info):
-    if type == 0:
+def main(window, type, sql_query_info):
+    output = type[1](sql_query_info)
+    if type[0] == 0:
         label = Label(window, text= f'Результаты по запросу "{sql_query_info[0]}":')
     else:
-        label = Label(window, text=f'Результаты по запросу №{type}:, {sql_query_info}')
+        label = Label(window, text=f'Результаты по запросу №{type[0]}:')
+
     label.pack(side='top')
 
     table_frame = Frame(window)
-    table_frame.pack()
+    table_frame.pack(fill='both')
+
 
 
     table = ttk.Treeview(table_frame, selectmode='browse')
-    table.pack(side='left')
+    table.pack(side='left',fill='both')
 
     # TODO: scrollbar style
     vert_bar = ttk.Scrollbar(table_frame, orient='vertical', command=table.yview)
     vert_bar.pack(side='right', fill='y')
 
-    table.configure(xscrollcommand= vert_bar.set)
+    table.configure(xscrollcommand=vert_bar.set)
 
+    button = Button(window, text='Назад', command=lambda: back_to_main_menu(window))
+    button.pack(anchor='se')
+    try:
+        (columns, values) = output
 
-    if type == 0:# TODO: backend function to get table
-        pass
-    elif type == 1:
-        pass
-    elif type == 1:
-        pass
-    elif type == 1:
-        pass
-    elif type == 1:
-        pass
-    elif type == 1:
-        pass
-    elif type == 1:
-        pass
-    else:
-        pass #error
-    table['columns'] = ('1','2','3')
-    table['show'] = 'headings'
-    table.column('1', width=90, anchor='c')
-    table.column('2', width=90, anchor='c')
-    table.column('3', width=90, anchor='c')
-    table.heading('1', text='rgd')
-    table.heading('2', text='sggbs')
-    table.heading('3', text='sdbdb')
-    for i in range(15):
-        table.insert('', 'end', text='L1', values=(f'{i}',f'{i+1}',f'{i-1}'))
+        table['columns'] = columns
+        table['show'] = 'headings'
+        for i in table['columns']:
+            table.column(i, width=(550//len(table['columns'])), anchor='c')
+            table.heading(i, text=i)
 
+        for i in values:
+            print(1, list(i))
+            table.insert('', 'end', values=list(i))
 
-    button = Button(window,text='Назад', command= lambda : back_to_main_menu(window))
-    button.pack()
+    except TypeError:
+        messagebox.showerror('Error', 'По данному запросу ничего не найдено')
+        return
 
 
